9d88a863760bf8c98288f8b46ec09721c29bbbf5
mudassarmshaikh <mudassar.shaikh@gmail.com>
2021-11-12 20:12:02 -0800

    Made XPath work
    But for loop does not work as expected.

--- mec-5.5.4-webscraping-project/toscrape-xpath.py
+++ mec-5.5.4-webscraping-project/toscrape-xpath.py
@@ -17,22 +17,24 @@ class QuotesSpider(scrapy.Spider):
     #       yield scrapy.Request(url=url, callback=self.parse)
 
     def start_requests(self):
-        url = 'http://quotes.toscrape.com/'
+        url = 'https://quotes.toscrape.com/'
+
         tag = getattr(self, 'tag', None)
         if tag is not None:
             url = url + 'tag/' + tag
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        for quote in response.xpath('//div/quote()'):
+        for quote in response.xpath("//div[@class='container']"):
+            print(quote)
             yield {
-                'text': quote.xpath('//span/text/text()').get(),
-                'author': quote.xpath('//smallauthor/text()').get(),
+                'text': quote.xpath('//div/span/text()').get(),
+                'author': quote.xpath('//div/span/small/text()').get(),
                 'tags': quote.xpath('//div/tags //a/tag/text()').getall(),
                 }
     
-        next_page = response.xpath('//li/next //a/attr(href)').get()
-        if next_page is not None:
+        #next_page = response.xpath('//li/next //a/attr(href)').get()
+        #if next_page is not None:
         #    next_page = response.urljoin(next_page)
         #    yield scrapy.Request(next_page, callback=self.parse)
-            yield response.follow(next_page, callback=self.parse)
+        #    yield response.follow(next_page, callback=self.parse)
